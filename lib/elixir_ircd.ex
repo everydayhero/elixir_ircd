9548cbd7573b781007773fac3af5236aab588c29
Mark Ryall <mark@ryall.name>
2014-09-02 22:50:15 +1000

    split out server

--- lib/elixir_ircd.ex
+++ lib/elixir_ircd.ex
@@ -1,6 +1,6 @@
 defmodule ElixirIrcd do
   def main(args) do
-    args |> parse_args |> process
+    args |> parse_args |> ElixirIrcd.Server.process
   end
 
   def parse_args(args) do
@@ -14,9 +14,5 @@ defmodule ElixirIrcd do
       { [ port: port], _, _} -> port
       _                      -> 6667
     end
-  end
-
-  def process(port) do
-    IO.puts "Listening on port #{port}"
   end
 end
